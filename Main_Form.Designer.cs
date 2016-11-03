4f81c777348341fc40c9c1c7b47d8a6db7ce0abc
geekloper <derfoufiabdel@gmail.com>
2016-11-03 20:07:32 +0100

    Version 1

---
+++ Main_Form.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Youtube_Mp3
+{
+    partial class Main_Form
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.Bt_Download = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.WebBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.TextBox1.Location = new System.Drawing.Point(68, 69);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(339, 35);
+            this.TextBox1.TabIndex = 1;
+            // 
+            // Bt_Download
+            // 
+            this.Bt_Download.Location = new System.Drawing.Point(413, 68);
+            this.Bt_Download.Name = "Bt_Download";
+            this.Bt_Download.Size = new System.Drawing.Size(127, 36);
+            this.Bt_Download.TabIndex = 2;
+            this.Bt_Download.Text = "Téléchargé";
+            this.Bt_Download.UseVisualStyleBackColor = true;
+            this.Bt_Download.Click += new System.EventHandler(this.Down_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(71, 164);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(469, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(288, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(349, 190);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Debit Bytes/Second : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(461, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 6;
+            // 
+            // WebBrowser1
+            // 
+            this.WebBrowser1.AllowNavigation = false;
+            this.WebBrowser1.AllowWebBrowserDrop = false;
+            this.WebBrowser1.Location = new System.Drawing.Point(71, 240);
+            this.WebBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.WebBrowser1.Name = "WebBrowser1";
+            this.WebBrowser1.ScriptErrorsSuppressed = true;
+            this.WebBrowser1.Size = new System.Drawing.Size(469, 65);
+            this.WebBrowser1.TabIndex = 0;
+            this.WebBrowser1.Url = new System.Uri("http://www.youtube-mp3.org/", System.UriKind.Absolute);
+            this.WebBrowser1.Visible = false;
+            this.WebBrowser1.WebBrowserShortcutsEnabled = false;
+            this.WebBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.WebBrowser1_DocumentCompleted);
+            this.WebBrowser1.NewWindow += new System.ComponentModel.CancelEventHandler(this.WebBrowser1_NewWindow);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(12, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "URL :";
+            // 
+            // Main_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(556, 334);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.Bt_Download);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.WebBrowser1);
+            this.MaximizeBox = false;
+            this.Name = "Main_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Convertisseur YouTube -> mp3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox1;
+        private System.Windows.Forms.Button Bt_Download;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.WebBrowser WebBrowser1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Main_Form.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+
+/**********************************************
+ * AUTHOR : ABDELLAH DERFOUFI 
+ * 
+ * GITHUB : http://github.com/geekloper
+ **********************************************
+ */
+
+namespace Youtube_Mp3
+{
+    public partial class Main_Form : Form
+    {
+
+        #region Members
+            private SaveFileDialog saveFileDialog1;
+            private WebClient webClient;
+            private DateTime lastUpdate;
+            private long lastBytes = 0;
+        #endregion
+
+        public Main_Form()
+        {
+            InitializeComponent();
+
+            WebBrowser1.Navigate("http://www.youtube-mp3.org/");
+            WebBrowser1.ScriptErrorsSuppressed = true;
+
+            webClient = new WebClient();
+            webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadFileCompleted);
+            webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+
+        }
+
+        private void Down_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                WebBrowser1.Document.GetElementById("youtube-url").SetAttribute("value", TextBox1.Text);
+                WebBrowser1.Document.GetElementById("submit").InvokeMember("click");
+
+                Stream myStream;
+
+                HtmlElement download_link = WebBrowser1.Document.GetElementById("dl_link");
+
+                HtmlElementCollection links = download_link.GetElementsByTagName("a");
+
+                HtmlElement error = WebBrowser1.Document.GetElementById("error_text");
+
+                if (links.Count <= 0 && this.WebBrowser1.ReadyState == WebBrowserReadyState.Complete)
+                {
+                    throw new Exception("Il est survenu une erreur depuis le site YouTube, nous ne pouvons pas traiter cette vidéo!\nLa plupart du temps, cette erreur provient de l’existence de droits réservés pour la vidéo ou bien de sa longueur.\nNous ne pouvons prendre en charge que des vidéos d’une durée maximum de 20 minutes.");
+                }
+                else
+                {
+                    for (int i = 0; i < links.Count; i++)
+                    {
+                        if (links[i].Style == null)
+                        {
+                            saveFileDialog1 = new SaveFileDialog();
+                            saveFileDialog1.RestoreDirectory = true;
+
+                            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                            {
+                                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                                {
+                                    myStream.Close();
+                                    webClient.DownloadFileAsync(new Uri(links[i].GetAttribute("href")), saveFileDialog1.FileName + ".mp3");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+                File.Delete(saveFileDialog1.FileName);
+                MessageBox.Show("Votre fichier a été téléchargé !","Félicitation !",MessageBoxButtons.OK,MessageBoxIcon.Asterisk);
+                progressBar1.Value = 0;
+                label1.Text = "";
+        }
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            label1.Text = e.ProgressPercentage.ToString() + "%";
+
+            long bytes = e.BytesReceived;
+
+            if (lastBytes == 0)
+            {
+                lastUpdate = DateTime.Now;
+                lastBytes = bytes;
+                return;
+            }
+
+            var now = DateTime.Now;
+            var timeSpan = now - lastUpdate;
+            if (timeSpan.Seconds == 0)
+            {
+                return;
+            }
+            var bytesChange = bytes - lastBytes;
+            var bytesPerSecond = bytesChange / timeSpan.Seconds;
+
+            lastBytes = bytes;
+            lastUpdate = now;
+
+            label3.Text = bytesPerSecond.ToString();
+
+        }
+
+        private void WebBrowser1_NewWindow(object sender, CancelEventArgs e)
+        {
+            WebBrowser1.Navigate(WebBrowser1.StatusText);
+            e.Cancel = true;
+        }
+
+        private void WebBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            WebBrowser1.Document.Window.Error += new HtmlElementErrorEventHandler(Window_Error);
+        }
+
+        private void Window_Error(object sender,HtmlElementErrorEventArgs e)
+        {
+            MessageBox.Show("Erreur !");
+            e.Handled = true;
+        }
+    }
+}
